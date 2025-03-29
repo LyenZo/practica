0c73bd9e52eb26dab5d8b885586a55cbecfd8e60
LyenZo <marcosj.9807@gmail.com>
2025-03-29 03:23:29 -0600

    ahhh

--- src/components/userList/LoginForm.jsx
+++ src/components/userList/LoginForm.jsx
@@ -16,7 +16,7 @@ const LoginForm = ({ onLoginSuccess, onLogout }) => {
         }
 
         try {
-            const response = await fetch('http://127.0.0.1:5000/users/login', {
+            const response = await fetch('http://34.202.231.19//users/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,6 +1,6 @@
 // src/services/userService.js
 
-const BASE_URL = "http://127.0.0.1:5000/";
+const BASE_URL = "http://34.202.231.19/";
 
 export async function getAllUsers() {
     try {
